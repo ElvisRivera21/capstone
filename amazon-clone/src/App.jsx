89008d65ace5a54cafbba4bfd501d4da2a3a47e5
luciano lawson <lucianoglawson@yahoo.com>
2024-02-22 18:48:30 -0600

    Co-authored-by: Dirty Disco <ElvisRivera21@users.noreply.github.com>

--- amazon-clone/src/App.jsx
+++ amazon-clone/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import './nav/login'
 
 function App() {
 
---
+++ amazon-clone/src/nav/login.jsx
@@ -0,0 +1,8 @@
+<div>
+    <h2>Login</h2>
+        <form onSubmit={handleSubmit}>
+        <label>Username:</label>
+        <label>Password:</label>
+        <input/>
+        </form>
+</div>
