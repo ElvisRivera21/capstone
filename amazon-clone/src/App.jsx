03cc49e309db340c0e490aa4a3a5f6ad44d25e6f
luciano lawson <lucianoglawson@yahoo.com>
2024-02-22 18:56:32 -0600

    new files

---
+++ amazon-clone/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import './nav/login'
+import './nav/register'
+
+
+
+function App() {
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
+    
---
+++ amazon-clone/src/nav/register.jsx
@@ -0,0 +1,7 @@
+<div>
+    <h2>Register</h2>
+        <form onSubmit={handleSubmit}>
+        <label>Username:</label>
+        <label>Password:</label>
+        </form>
+</div>
