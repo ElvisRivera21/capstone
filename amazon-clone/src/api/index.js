f60b9b06e6d105d5f7497aa8b1cb9a916df1a6d1
luciano lawson <lucianoglawson@yahoo.com>
2024-02-29 19:45:42 -0600

    updated api js files

--- amazon-clone/src/api/index.js
+++ amazon-clone/src/api/index.js
@@ -1,23 +1,5 @@
-import { PrismaClient } from '@prisma/client';
-require('dotenv').config();
+const router = require("express").Router();
 
-const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.database_url,
-    },
-  },
-});
+router.use("/user:id", require("./user:id"));
 
-async function main() {
-  const allUsers = await prisma.user.findMany();
-  console.log(allUsers);
-}
-
-main()
-  .catch((e) => {
-    throw e;
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+module.exports = router;
