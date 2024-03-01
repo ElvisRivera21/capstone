c9020036810af26ca03c787c7010e9fef5c256cc
luciano lawson <lucianoglawson@yahoo.com>
2024-02-29 19:24:27 -0600

    added .env file

--- amazon-clone/src/api/index.js
+++ amazon-clone/src/api/index.js
@@ -1,15 +1,23 @@
 import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
+require('dotenv').config();
+
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.database_url,
+    },
+  },
+});
 
 async function main() {
-    const allUsers = await prisma.user.findMany();
-    console.log(allUsers);
+  const allUsers = await prisma.user.findMany();
+  console.log(allUsers);
 }
 
 main()
-    .catch(e => {
-        throw e;
-    })
-    .finally(async () => {
-        await prisma.$disconnect();
-    });
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
