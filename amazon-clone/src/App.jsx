6bb5089bc78251b844b156f5efb2852c412081d2
Sophia <sophiaredd004@gmail.com>
2024-03-03 21:26:13 -0500

    updated code

--- amazon-clone/src/App.jsx
+++ amazon-clone/src/App.jsx
@@ -1,17 +1,21 @@
-import './App.css'
-import './nav/login'
-import './nav/register'
+document.getElementById("search-input").addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+      event.preventDefault(); 
+
+      var query = document.getElementById("search-input").value;
+
+
+      var xhr = new XMLHttpRequest();
+      xhr.open("GET", "search.php?q=" + encodeURIComponent(query), true);
+      xhr.onload = function() {
+          if (xhr.status == 200) {
+              
+              document.getElementById("search-results").innerHTML = xhr.responseText;
+          }
+      };
+      xhr.send();
+  }
+});
 
 
 
-function App() {
-
-  return (
-    <>
-
-    </>
-  )
-}
-
-export default App
-    
