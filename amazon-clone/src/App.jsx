ef625f1ee2ebf228ec599125ef6dcb0ae77edbe1
luciano lawson <lucianoglawson@yahoo.com>
2024-03-04 19:07:34 -0600

    re-organized files and created new route/path files

--- amazon-clone/src/App.jsx
+++ amazon-clone/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
-import '../config/nav/login'
-import '../config/nav/register'
-import '../db/index'
+import './config/nav/login'
+import './config/nav/register'
+import './db/index'
 
 function App() {
 
---
+++ amazon-clone/src/Routes.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Cart from './pages/cart';
+import Clothing from './pages/clothing';
+import Groceries from './pages/groceries';
+import Home from './pages/home';
+import HomeImprovements from './pages/homeImprovements';
+import Kids from './pages/kids';
+import Medical from './pages/medical';
+import Womens from './pages/womens';
+
+function AppRoutes() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/clothing" element={<Clothing />} />
+        <Route path="/groceries" element={<Groceries />} />
+        <Route path="/homeImprovements" element={<HomeImprovements />} />
+        <Route path="/kids" element={<Kids />} />
+        <Route path="/medical" element={<Medical />} />
+        <Route path="/womens" element={<Womens />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default AppRoutes;
