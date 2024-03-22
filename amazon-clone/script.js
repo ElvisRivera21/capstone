4275d799202fc627384e71d322d29ba3e370e085
luciano lawson <lucianoglawson@yahoo.com>
2024-03-21 20:08:56 -0500

    new files

--- amazon-clone/script.js
+++
@@ -1,6 +0,0 @@
-//login function
-function openLogin() {
-    // Display the login form when the "My Account" button is clicked
-    document.getElementById('loginForm').style.display = 'block';
-}
-
--- amazon-clone/src/api/db/signup.js
+++
@@ -1,8 +0,0 @@
-import db from './client';
-
-const registerUser = async (userDetails) => {
-    // userDetails should include necessary user info
-    return db.one('INSERT INTO users(name, email, password) VALUES($1, $2, $3) RETURNING id', [userDetails.name, userDetails.email, userDetails.password]);
-};
-
-export default registerUser;
